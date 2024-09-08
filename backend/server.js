5cd4919dec02e214e7a09a591a25dd57311fe49a
ABDUL REHMAN <abdulrehman.asif2001@gmail.com>
2024-09-08 23:11:13 +0500

    deploy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import multipart from 'connect-multiparty';
+import path from 'path'
 
 
 import authRoutes from './routes/auth.routes.js'
@@ -15,6 +16,8 @@ import connectDB from './db/connectDB.js';
 dotenv.config();
 const port =process.env.PORT || 5000;
 
+const __dirname = path.resolve()
+
 // app.get('/', (req,res)=>res.send("All OK!🤘🏻"))
 //to parse the incomming data with json payload (from req.body)
 app.use(multipart());
@@ -25,6 +28,13 @@ app.use(cookieParser())
 app.use('/api/auth',authRoutes)
 app.use ('/api/messages',messageRoutes)
 app.use('/api/users', userRoutes)
+
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen( port ,()=> {
     connectDB();    
